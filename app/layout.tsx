35ee8c5ab98492380c5e39a8157fe6e7a5c574e1
Gabriel Rocha <rochafrgabriel@gmail.com>
2025-10-16 22:55:10 -0300

    update with some personal data

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ export const metadata: Metadata = {
     canonical: '/'
   },
   title: {
-    default: 'John Smith',
-    template: '%s | John Smith'
+    default: 'Gabriel Rocha',
+    template: '%s | Gabriel Rocha'
   },
-  description: 'My portfolio, blog, and personal website.'
+  description: 'My personal portfolio, blog, and personal website.'
 };
 
 export default function RootLayout({
@@ -39,25 +39,29 @@ export default function RootLayout({
 
 function Footer() {
   const links = [
-    { name: '@johnsmith', url: 'https://x.com/johnsmith' },
-    { name: 'youtube', url: 'https://www.youtube.com/@johnsmith' },
-    { name: 'linkedin', url: 'https://www.linkedin.com/in/johnsmith' },
-    { name: 'github', url: 'https://github.com/johnsmith' }
+    { name: 'email', url: 'mailto:contatogabrielrochaf@gmail.com' },
+    { name: 'linkedin', url: 'https://www.linkedin.com/in/gabrielforster' },
+    { name: 'github', url: 'https://github.com/gabrielforster' }
   ];
 
   return (
     <footer className="mt-12 text-center">
-      <div className="flex justify-center space-x-4 tracking-tight">
-        {links.map((link) => (
-          <a
-            key={link.name}
-            href={link.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-400 dark:text-gray-500 hover:text-blue-500 transition-colors duration-200"
-          >
-            {link.name}
-          </a>
+      <div className="flex justify-center space-x-4 tracking-tight text-gray-400 dark:text-gray-500">
+        {links.map((link, index) => (
+          <div key={link.name}>
+            <a
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-500 transition-colors duration-200"
+            >
+              {link.name}
+            </a>
+
+            {(index < links.length - 1) && (
+              <span className="ml-4">/</span>
+            )}
+          </div>
         ))}
       </div>
     </footer>
